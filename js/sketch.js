082fe89493fbfd5cd9a600c386eae1294f4ce13c
KieranCoopr <kieran.cooper@students.plymouth.ac.unknown>
2021-11-12 16:40:51 +0000

    created a emoji with googley eyes.

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,51 @@
+var canvasSize = 400;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(canvasSize, canvasSize);
     canvas.parent("p5container");
 }
 
 function draw() {
+
+    var eyeX = map(mouseX, 0, canvasSize,125,175)
+    var eyeY = map(mouseY, 0, canvasSize,125,175)
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    //console.log(mouseX,mouseY)
+
+    stroke(100)
+    strokeWeight(10)
+
+    //head
+    fill(255, 255, 0);
+    ellipse(width / 2, height/2, 300);
+
+    //eyes
+    fill(255);
+    ellipse(eyeX,eyeY,50,60)
+    ellipse(eyeX+100,eyeY,50,60)
+
+    //pupils
+    
+    
+    //cheeks
+    noStroke()
+
+    fill(255,215,0)
+    ellipse(110,220,45)
+    ellipse(290,220,45)
+    
+    //smile
+
+
+    //fill(100)
+    //ellipse(200,260,100,50)
+    //fill(255,255,0)
+
+    //ellipse(200,250,100,50)
+
+    stroke(100)
+    strokeWeight(10)
+    noFill()
+    arc( 200, 220, 180, 180, radians(30), radians(150))
 }
